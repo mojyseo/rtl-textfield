9147bbeb17b008a4d8b0535d8eaed036ddbe8c4f
mojtaba madadi <mojyseo@gmail.com>
2022-11-06 03:06:51 +0330

    adapt mui

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react'
 import styles from './styles.module.css'
-import TextField from '@material-ui/core/TextField'
+import TextField from '@mui/material/TextField'
 
 export default function ExampleComponent(props) {
-  return (
-    <TextField
-      dir='rtl'
-      className={styles.textfieldRtl}
-      {...props}
-    />
-  )
+  return <TextField dir='rtl' className={styles.textfieldRtl} {...props} />
 }
