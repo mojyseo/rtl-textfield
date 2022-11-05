06fff19c51cfaa440178e3c0a0f2f60278e30aaf
mojyseo <60144572+mojyseo@users.noreply.github.com>
2022-11-05 18:49:56 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,37 +6,8 @@ export default function ExampleComponent(props) {
   return (
     <TextField
       dir='rtl'
-      autoComplete={props.autoComplete}
-      autoFocus={props.autoFocus}
-      classes={props.classes}
-      color={props.color}
-      defaultValue={props.defaultValue}
-      disabled={props.disabled}
-      error={props.error}
-      FormHelperTextProps={props.FormHelperTextProps}
-      fullWidth={props.fullWidth}
-      helperText={props.helperText}
-      id={props.id}
-      InputLabelProps={props.InputLabelProps}
-      inputProps={props.inputProps}
-      InputProps={props.InputProps}
-      inputRef={props.inputRef}
-      label={props.label}
-      margin={props.margin}
-      multiline={props.multiline}
-      name={props.name}
-      onChange={props.onChange}
-      placeholder={props.placeholder}
-      required={props.required}
-      rows={props.rows}
-      rowsMax={props.rowsMax}
-      select={props.select}
-      SelectProps={props.SelectProps}
-      size={props.size}
-      type={props.type}
-      value={props.value}
-      variant={props.variant}
       className={styles.textfieldRtl}
+      {...props}
     />
   )
 }
